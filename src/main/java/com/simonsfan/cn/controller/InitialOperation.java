053c7bcbaa730b2b91cbc646a223e739a108c219
simonsfan <17802115971@163.com>
2018-04-13 17:42:03 +0800

    code fixed

--- src/main/java/com/simonsfan/cn/controller/InitialOperation.java
+++ src/main/java/com/simonsfan/cn/controller/InitialOperation.java
@@ -1,21 +1,20 @@
 package com.simonsfan.cn.controller;
 
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
 /**
  * <p>
- * 系统初始化加载数据，例如初始化数据到redis中
+ * 系统初始化加载数据，例如初始化 热数据到redis中
  * </p>
  * Created by fanrx on 2018/3/11.
  */
-@Controller
-//@RequestMapping("/init")
+@Component
 public class InitialOperation implements InitializingBean {
 
     public void afterPropertiesSet() {
-        System.out.println("this is the initial content……");
-        //TODO
+        System.out.println("this is the initial operation……");
+        //TODO something else……
     }
 
 
