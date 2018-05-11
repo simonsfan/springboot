bb7896ece477146263e8d39bdd5de9e2eb95d545
simonsfan <17802115971@163.com>
2018-05-11 17:41:17 +0800

    增加springboot默认首页

---
+++ src/main/java/com/simonsfan/cn/controller/DefaultView.java
@@ -0,0 +1,20 @@
+package com.simonsfan.cn.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by fanrx on 2018/5/11.
+ */
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
---
+++ src/main/java/com/simonsfan/cn/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.simonsfan.cn.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by fanrx on 2018/5/11.
+ */
+@Controller
+public class IndexController {
+/*
+    @RequestMapping("/")
+    public String index(Model model, HttpServletResponse response) {
+        model.addAttribute("name", "simonsfan");
+        return "/index";
+    }*/
+
+}
