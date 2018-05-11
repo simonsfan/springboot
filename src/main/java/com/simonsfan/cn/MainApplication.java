a56e7576419f3a3520042fd49d00b808d285df05
simonsfan <17802115971@163.com>
2018-05-11 17:54:19 +0800

    将springboot打包成war包，方便Tomcat跑

--- src/main/java/com/simonsfan/cn/MainApplication.java
+++ src/main/java/com/simonsfan/cn/MainApplication.java
@@ -2,16 +2,32 @@ package com.simonsfan.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by fanrx on 2018/3/6.
  */
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
-@SpringBootApplication
-public class MainApplication {
+//@SpringBootApplication
+/*public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+}*/
+
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
     }
 
 }
