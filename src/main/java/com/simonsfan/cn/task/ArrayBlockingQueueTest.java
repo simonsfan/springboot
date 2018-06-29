7bec5d564a710b6d123935043a2568cbc63697b8
simonsfan <17802115971@163.com>
2018-06-29 21:43:15 +0800

    代码修改

--- src/main/java/com/simonsfan/cn/task/ArrayBlockingQueueTest.java
+++ src/main/java/com/simonsfan/cn/task/ArrayBlockingQueueTest.java
@@ -1,7 +1,42 @@
 package com.simonsfan.cn.task;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
  * Created by fanrx on 2018/6/10.
  */
 public class ArrayBlockingQueueTest {
+
+
+    static   final  List<String> list = new ArrayList<String>() {
+            {
+                add("wangwu");
+                add("lisi");
+                add("zhangsan");
+            }
+        };
+static   final Map<String,String> map = new HashMap<String,String>() {
+            {
+                put("1","wangwu");
+                put("2","lisi");
+                put("3","zhangsan");
+            }
+        };
+
+    public static void main(String[] args) throws InterruptedException {
+        ArrayBlockingQueue queue = new ArrayBlockingQueue(2000);
+        queue.offer(map);
+
+        boolean add = queue.add(list);
+        System.out.println(add);
+        Object take1 = queue.peek();
+        System.out.println(take1);
+
+//        Object take = queue.poll();
+//        System.out.println(take);
+    }
 }
